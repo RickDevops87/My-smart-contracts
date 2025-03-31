6f62d9f50373f1604ad0ce20876172ec2331f3e5
RickDevops87 <rick.devops87@gmail.com>
2025-03-31 13:13:12 +0200

    Create StorageType.sol

---
+++ StorageType.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract SimpleStorage {
+    // Basic types
+        bool hasFavoriteNumber = true;
+            uint256 favoriteNumber = 88;
+                string favoriteNumberInText = "eighty-eight";
+                    int256 $favoriteInt = -88;
+                        address myAddress =0xAB1b7206aa6840C795aB7A6AE8b15417B7E63A8D ;
+                            bytes32 favoriteBytes32 = "cat";
+}
